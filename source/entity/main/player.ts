9d7fda4322008b7dad4c3db8a3532f4417458062
Ziphil <yfe23457@nifty.com>
2022-09-11 19:21:31 +0900

    プレイヤーの基本処理を追加

--- source/entity/main/player.ts
+++ source/entity/main/player.ts
@@ -3,8 +3,12 @@
 import {
   Circle,
   Color,
+  Engine,
   vec
 } from "excalibur";
+import {
+  InputManagerComponent
+} from "/source/component";
 import {
   FloatingActor
 } from "/source/entity/floating-actor";
@@ -26,6 +30,23 @@ export class Player extends FloatingActor {
       pos: vec(tileX * TILE_DIMENSTION.width, tileY * TILE_DIMENSTION.height)
     });
     this.graphics.use(new Circle({radius: 8, color: Color.fromHex("#000000")}));
+  }
+
+  public override onInitialize(engine: Engine): void {
+    this.initializeComponents(engine);
+  }
+
+  public override onPreUpdate(engine: Engine, delta: number): void {
+    this.move(delta);
+  }
+
+  private initializeComponents(engine: Engine): void {
+    const inputComponent = new InputManagerComponent();
+    this.addComponent(inputComponent);
+  }
+
+  private move(delta: number): void {
+    const inputManager = this.get(InputManagerComponent)!;
   }
 
 }
